cc31e896919059081638c6995c60adb75059dcdb
bavly-ashraf <bavly.ashraf2023@gmail.com>
2024-08-17 23:34:52 +0300

    showing loading indicator in homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
@@ -8,13 +8,20 @@ import welcomeImg from './assets/welcome-image.svg';
 
 function App() {
   const contacts = useSelector(state => state.contacts.value);
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
 
   useEffect(()=>{
     (async function(){
       if(!contacts.length){
-        const contactsFromAPI = (await axios.get('https://randomuser.me/api/?results=50'))?.data?.results;
-        dispatch(addAllContacts(contactsFromAPI));
+        try{
+          setLoading(true)
+          const contactsFromAPI = (await axios.get('https://randomuser.me/api/?results=50'))?.data?.results;
+          dispatch(addAllContacts(contactsFromAPI));
+          setLoading(false);
+        }catch(e){
+          alert(e);
+        }
       }
     })();
   });
@@ -27,6 +34,11 @@ function App() {
         <div className='flex gap-16'>
           <Link to={'contact-list'} className="btn btn-info">Contacts list</Link>
           <Link to={'contact-actions'} className="btn btn-warning">Contacts actions</Link>
+        </div>
+      </div>
+      <div className="toast">
+        <div className={loading? 'alert alert-info': contacts.length > 0? 'alert alert-success':'alert alert-error'}>
+          <span>{loading? 'Contacts are being loaded...': contacts.length > 0? 'Contacts are loaded successfully.':'There\'s an error!'}</span>
         </div>
       </div>
     </>
